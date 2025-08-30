a0dab7419509efac338815d4f26c061960311b94
anos7voldigoad <pak1puresalt@gmail.com>
2025-08-30 07:26:59 +0500

    Updated design and fixes

--- src/components/ConsultationModal.tsx
+++ src/components/ConsultationModal.tsx
@@ -32,7 +32,7 @@ const ConsultationModal: React.FC<ConsultationModalProps> = ({ isOpen, onClose }
   };
 
 const SITE_KEY = "6LcRrrcrAAAAAFsPoitByrEX6gd6PBgbRfmm_yuc"; // reCAPTCHA v3 site key
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwQP2v-vgeShD5u3yZTymlFtjS24cFNTekQ-NO9TvTnH8Yc8pkR9sbTrw7gh_YdMGyZYA/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzPjqhZOgJP3KYXuCDD6Kd61YCfR2aoIUQ4RefEtRpAnktuHEBECzbNUB3cF20Q_76KwA/exec";
 
 const handleSubmit = async (e: React.FormEvent) => {
   e.preventDefault();
